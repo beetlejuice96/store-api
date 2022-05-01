fe9fefad7ac287493db4c295847c51ae4d6dc70b
beetlejuice96 <dilanbernbae043@gmail.com>
2022-05-01 12:58:14 -0300

    add validators

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //quita los atributos que no esten definidos en el dto.
+      forbidNonWhitelisted: true, // retorna badrequest y avisa que se estan enviando parametros incorrectos.
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
